0a678feff111a90052bc295085d68ba167c99c71
katstasaph <katherinestasaph@gmail.com>
2021-12-11 10:42:12 -0500

    refactored, removed late night goof

--- advent11.rb
+++ advent11.rb
@@ -1,7 +1,4 @@
 MAX_ENERGY = 9
-# todo: make it stop after it finds a simultaneous flash instead of just increasing this constant manually and hoping
-# and also make it retun the number of flashes after 100
-TOTAL_STEPS = 250
 NON_OCTOPI = [:flashes, :simultaneous]
 
 def initialize_state(octopi)
@@ -23,7 +20,11 @@ def step!(octopi, step_num)
     end
   end
   do_flashes!(octopi, step_num)
-  if octopi.values.all? { |value| value.is_a?(Integer) || value[:counter] == 0 } && octopi[:simultaneous] < 0  then octopi[:simultaneous] = (step_num + 1) end    
+  if first_simultaneous_flash?(octopi) then octopi[:simultaneous] = (step_num) end    
+end
+
+def first_simultaneous_flash?(octopi)
+  octopi.values.all? { |value| value.is_a?(Integer) || value[:counter] == 0 } && octopi[:simultaneous] < 0 
 end
 
 def increment_counter!(octopus)
@@ -31,16 +32,24 @@ def increment_counter!(octopus)
 end
 
 def do_flashes!(octopi, step_num)
-  if octopi.values.all? { |value| value.is_a?(Integer) || value[:counter] <= 9 || value[:flashed] }
-    octopi.values.each { |value| if value.is_a?(Hash) then value[:flashed] = false end }
+  if done_flashing?(octopi)
+    reset_octopi!(octopi)
     return
-  end
+  end  
   octopi.values.each do |value|
     if value.is_a?(Hash)
       if should_flash?(value) then flash!(octopi, value) end
    end
   end
   do_flashes!(octopi, step_num)
+end
+
+def done_flashing?(octopi)
+  octopi.values.all? { |value| value.is_a?(Integer) || value[:counter] <= 9 || value[:flashed] }
+end
+
+def reset_octopi!(octopi)
+  octopi.values.each { |value| if value.is_a?(Hash) then value[:flashed] = false end }
 end
 
 def should_flash?(octopus)
@@ -65,30 +74,12 @@ def adjacent?(octopi, octopus)
   octopi[:coords] != octopus[:coords] && (octopi[:coords][0] - octopus[:coords][0]).abs <= 1 && (octopi[:coords][1] - octopus[:coords][1]).abs <= 1
 end
 
-def breed_lanternfish(fish)
-  days = 1 
-  while days <= DAYS_TO_BREED
-    next_days_fish = fish.dup
-    next_days_fish[-1] = 0
-    fish.each do |counter, count|
-      next_days_fish[counter - 1] = count
-      next_days_fish[counter] = (count - 1)
-    end
-    fish = next_days_fish
-    if fish[-1]
-      fish[INITIAL_COUNTER] += fish[-1]
-      fish[CHILD_COUNTER] = fish[-1]
-      fish.delete(-1)
-    end
-    days += 1
-  end
-  fish.values.inject(:+)
-end
-
 octopi_data = File.read('advent11.txt').split.map { |line| line.chars.map { |chr| chr.to_i } }
 octopi_hash = initialize_state(octopi_data)
-TOTAL_STEPS.times do |step_num|
-  step!(octopi_hash, step_num)
+step_num = 1
+until octopi_hash[:simultaneous] > 0
+  step!(octopi_hash, step_num) 
+  if step_num == 100 then puts "Total flashes after 100 steps: #{octopi_hash[:flashes]}" end
+  step_num += 1
 end
-p octopi_hash[:flashes]
-p octopi_hash[:simultaneous]
+puts "First simultaneous flash occurred on step #{octopi_hash[:simultaneous]}"
